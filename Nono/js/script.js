ff031df192f29f5897a1082029a53a34878938dd
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-11-11 19:30:57 -0300

    nono

--- Nono/js/script.js
+++ Nono/js/script.js
@@ -1,23 +1,31 @@
 var mm = 0
 var ss = 0
+var ip = 0
+var ii = 0
 var contadore = 0
 function iniciar(){
 var texto = document.getElementById("texto")
     contadore = setInterval(contador,100)
     texto.innerHTML = 'Contando'
-
+    ii = 1
 }
 function reset(){
     mm = document.getElementById("mm")
     ss = document.getElementById("ss")
     mm.innerHTML = '00'
     ss.innerHTML = '00'
-
+    if(ip != 0 && ii != 1 ){
+    texto.innerHTML = 'Zerado'
+    ip = 0
+    
+}
 }
 function parar(){
     var texto = document.getElementById("texto")
-    clearInterval(contadore)
-    texto.innerHTML = 'Parado'
+    clearInterval(contadore) 
+    texto.innerHTML = 'Parado'  
+    ip = 1
+    ii = 0
 }
 function contador(){
     mm = document.getElementById("mm")
