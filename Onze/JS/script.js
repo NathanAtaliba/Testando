8040cb0f1e6fb659dcf11034d4e561f19f50c52a
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-01-05 23:15:29 -0300

    Onze

--- Onze/JS/script.js
+++ Onze/JS/script.js
@@ -1,14 +1,20 @@
 function register(){
-nome = document.getElementById("nome");
-password = document.getElementById("password");
-email = document.getElementById("email");
+var nome = document.getElementById("nome");
+var password = document.getElementById("password");
+var email = document.getElementById("email");
 //sex = document.getElementById("sex");
 //hours = document.getElementById("hours");
 
 if(!nome.value || !password.value || !email.value ){
 window.alert("Preencha todos os campos!");
 }
+else{
+    
+    console.log("nome: " + nome.value);
+    console.log("Senha: " + password.value);
+    console.log("Email: " + email.value);
 
+}
 }
 function deleteData(){
     nome = document.getElementById("nome");
